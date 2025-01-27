5067fd96a7bc9484d9284594ff50d85d1ca5f188
Rachel Sheikh <sheikhrachel97@gmail.com>
2025-01-27 11:02:11 -0800

    fix: license

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 /*
-Copyright 2023 Rachel Sheikh
+Copyright 2023-2025 Rachel Sheikh
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy of
 	this software and associated documentation files (the “Software”), to deal in the
